079c45d8cf2e34ff5812acb76f38500f69a55b1a
Max Penet <m@qbits.cc>
2012-11-30 00:31:18 +0100

    fix clojure dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fetch "0.1.0-alpha2"
+(defproject cc.qbits/fetch "0.1.0-alpha2"
   :description "A ClojureScript and Noir library to make client-server interaction painless."
-  :dependencies [[clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.2.2"]])
